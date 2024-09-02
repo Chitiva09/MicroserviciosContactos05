a7f0aba76e73b4b7dbc16a96b7a7b7c1dfc8d065
Chitiva09 <bermeocamilo12@gmail.com>
2024-09-02 11:36:56 -0500

    esta si es la vencida

--- src/main/java/com/example/microservicio_cliente_contacto_05/controller/PersonasController.java
+++ src/main/java/com/example/microservicio_cliente_contacto_05/controller/PersonasController.java
@@ -16,30 +16,30 @@ import com.example.microservicio_cliente_contacto_05.model.Persona;
 @RestController
 public class PersonasController {
     @Autowired
-    RestTemplate templete;
+    RestTemplate template;
 
     String url = "http://localhost:8080";
 
     @GetMapping(value = "/personas/{nombre}/{email}/{edad}", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Persona> altaPersona(@PathVariable("nombre") String nombre,
-    @PathVariable("email") String email, @PathVariable("edad") int edad){
+            @PathVariable("email") String email, @PathVariable("edad") int edad) {
 
-        Persona persona = new Persona (nombre, email, edad);
+        Persona persona = new Persona(nombre, email, edad);
 
-        //templete.postForLocation(url,"/persona"+persona);
-        templete.postForLocation(url + "/persona", persona);
+        // template.postForLocation(url,"/persona"+persona);
+        template.getForObject(url + "/persona", Persona[].class);
 
-        Persona[] personas = templete.getForObject(url + "/contactos", Persona[].class);
-        return Arrays.asList(personas) ;
+        Persona[] personas = template.getForObject(url + "/contactos", Persona[].class);
+        return Arrays.asList(personas);
     }
 
-    @GetMapping(value= "/personas/{edad1}/{edad2}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Persona> buscarEdades (@PathVariable("edad1") int edad1, @PathVariable("edad2")int edad2){
-        Persona[] personas = templete.getForObject(url+"contactos", Persona[].class);
+    @GetMapping(value = "/personas/{edad1}/{edad2}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Persona> buscarEdades(@PathVariable("edad1") int edad1, @PathVariable("edad2") int edad2) {
+        Persona[] personas = template.getForObject(url + "contactos", Persona[].class);
         return Arrays.stream(personas)
-             .filter(p -> p.getEdad() >= edad1 && p.getEdad() <= edad2)
-             .collect(Collectors.toList());
-        
-       // Arrays.stream(personas).filter(p->p.getEdad()>-edad1&&p.getEdad()<-edad2).collect(Collectors.toList());
+                .filter(p -> p.getEdad() >= edad1 && p.getEdad() <= edad2)
+                .collect(Collectors.toList());
+
+        // Arrays.stream(personas).filter(p->p.getEdad()>-edad1&&p.getEdad()<-edad2).collect(Collectors.toList());
     }
 }
